c1942f63a77224d5a66e56092f5cde87f5dda2da
yuvalkansal <yuvalkansal@gmail.com>
2020-06-02 16:19:42 +0800

    Build command

---
+++ build/precache-manifest.60c02f07f33bbe8f44205cdc4884079d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b9bc9f93df4e6279719ca2cbef6ead2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cacf42a1a9686a3717e5",
+    "url": "/static/js/2.f2936b3e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f2936b3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba36ab6aca84846702fe",
+    "url": "/static/js/main.1940ed6e.chunk.js"
+  },
+  {
+    "revision": "c815442ab336b747f527",
+    "url": "/static/js/runtime-main.94a6af97.js"
+  },
+  {
+    "revision": "159c7813ced5afbfdb680c0f1eccca9b",
+    "url": "/static/media/profile_pic.159c7813.jpg"
+  }
+]);
