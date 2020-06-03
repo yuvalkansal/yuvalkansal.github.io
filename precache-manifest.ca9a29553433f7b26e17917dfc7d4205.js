af6d7d22adb7731532f57ac616e1083a5df6eb58
yuvalkansal <yuvalkansal@gmail.com>
2020-06-03 20:02:06 +0800

    Updates

---
+++ precache-manifest.ca9a29553433f7b26e17917dfc7d4205.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55587bd45f37abab2b73e607d6cc727c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72999aab17fef310c8b9",
+    "url": "/static/js/2.f0db3cd7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f0db3cd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0de1c29acb46ba00083a",
+    "url": "/static/js/main.8f3b47c9.chunk.js"
+  },
+  {
+    "revision": "c815442ab336b747f527",
+    "url": "/static/js/runtime-main.94a6af97.js"
+  },
+  {
+    "revision": "5528ee13102ce2a42cc2a125709cd942",
+    "url": "/static/media/profile_pic.5528ee13.jpg"
+  }
+]);
