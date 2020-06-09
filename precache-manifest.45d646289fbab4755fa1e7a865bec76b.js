c5df25cee7bff8d3b2decb1b148afb657d7078a4
yuvalkansal <yuvalkansal@gmail.com>
2020-06-09 19:52:37 +0800

    Updates

---
+++ precache-manifest.45d646289fbab4755fa1e7a865bec76b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "032b1ca4e23d4bb51b80ee35b299e605",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c4934d9883e60253773",
+    "url": "/static/js/2.7888aa89.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7888aa89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e2c336910663bef1a6d",
+    "url": "/static/js/main.0e86c8ab.chunk.js"
+  },
+  {
+    "revision": "c815442ab336b747f527",
+    "url": "/static/js/runtime-main.94a6af97.js"
+  },
+  {
+    "revision": "4b17c2d56fb765de3d607905e2a3841b",
+    "url": "/static/media/Baseball.4b17c2d5.jpg"
+  },
+  {
+    "revision": "50cb376eeeababb212c09511b5e7d1c8",
+    "url": "/static/media/SMO.50cb376e.jpg"
+  },
+  {
+    "revision": "2546a52339587d05cbacfceffba6f1c8",
+    "url": "/static/media/YuvalResume.2546a523.pdf"
+  },
+  {
+    "revision": "34436c59a8e02efe1ba12a7cabaf7b32",
+    "url": "/static/media/movie.34436c59.mp4"
+  },
+  {
+    "revision": "5528ee13102ce2a42cc2a125709cd942",
+    "url": "/static/media/profile_pic.5528ee13.jpg"
+  }
+]);
